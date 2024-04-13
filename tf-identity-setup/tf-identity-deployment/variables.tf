b9432b5fc79935b0a1b44d5c49745e2f2716bc1f
rash-ms <rashmujeeb@yahoo.com>
2024-04-13 23:43:51 +0100

    create organization unit

--- tf-identity-setup/tf-identity-deployment/variables.tf
+++ tf-identity-setup/tf-identity-deployment/variables.tf
@@ -1,14 +1,14 @@
-variable "team_accounts" {
-  type = map(object({
-    name  = string
-    email = string
-  }))
-}
+# variable "team_accounts" {
+#   type = map(object({
+#     name  = string
+#     email = string
+#   }))
+# }
 
-variable "teams" {
-  type = list(object({
-    name     = string
-    unit     = string
-    accounts = list(string)
-  }))
-}
+# variable "teams" {
+#   type = list(object({
+#     name     = string
+#     unit     = string
+#     accounts = list(string)
+#   }))
+# }
