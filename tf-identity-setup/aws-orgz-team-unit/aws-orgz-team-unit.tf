8270421c9b4c2522918a41d86771f0e2a3361240
rash-ms <rashmujeeb@yahoo.com>
2024-04-14 13:09:08 +0100

    rename folder

--- tf-identity-setup/aws-orgz-team-unit/aws-orgz-team-unit.tf
+++ tf-identity-setup/aws-orgz-team-unit/aws-orgz-team-unit.tf
@@ -1,27 +1,27 @@
 resource "aws_organizations_organization" "organization" {}
 
 resource "aws_organizations_organizational_unit" "data_team" {
-  name     = "data-team"
+  name      = "data-team"
   parent_id = aws_organizations_organization.organization.id
 }
 
 resource "aws_organizations_organizational_unit" "prod" {
-  name     = "Prod"
+  name      = "Prod"
   parent_id = aws_organizations_organizational_unit.data_team.id
 }
 
 resource "aws_organizations_organizational_unit" "non_prod" {
-  name     = "Non-prod"
+  name      = "Non-prod"
   parent_id = aws_organizations_organizational_unit.data_team.id
 }
 
 resource "aws_organizations_organizational_unit" "stg" {
-  name     = "stg"
+  name      = "stg"
   parent_id = aws_organizations_organizational_unit.non_prod.id
 }
 
 resource "aws_organizations_organizational_unit" "dev" {
-  name     = "dev"
+  name      = "dev"
   parent_id = aws_organizations_organizational_unit.non_prod.id
 }
 
@@ -30,33 +30,29 @@ resource "aws_organizations_organizational_unit" "dev" {
 # }
 
 # resource "aws_organizations_organizational_unit" "team" {
-#   for_each = { for team in var.teams : team.name => team.unit }
-
-#   name     = each.value
+#   for_each = locals.all_teams
+#   name     = each.key
 #   parent_id = aws_organizations_organizational_unit.non_prod.id
 # }
 
 # resource "aws_organizations_account" "team_accounts" {
-#   for_each = {
-#     for team_name, team in local.all_teams : team_name => team.accounts
-#   }
-
-#   name      = "${team_accounts.value}-${each.key}"
-#   email     = var.team_accounts[each.key].email
+#   for_each = local.all_teams
+#   name     = "${each.key}-account"
+#   email    = var.team_accounts[each.key].email
 #   parent_id = aws_organizations_organizational_unit.team[each.key].id
 # }
 
-# # Define the email addresses for each account in a JSON file
 # data "external" "team_account_emails" {
 #   program = ["bash", "-c", "cat ${path.module}/team_account_emails.json"]
 # }
 
 # locals {
 #   accounts = flatten([
-#     for team in var.teams_and_accounts : [
+#     for team in var.teams : [
 #       for account in team.accounts : {
 #         name  = account
 #         email = jsondecode(data.external.team_account_emails.result)[account]
+#         unit  = aws_organizations_organizational_unit.team[team.name].id
 #       }
 #     ]
 #   ])
@@ -64,8 +60,39 @@ resource "aws_organizations_organizational_unit" "dev" {
 
 # resource "aws_organizations_account" "accounts" {
 #   for_each = { for account in local.accounts : account.name => account }
-
-#   name       = each.key
-#   email      = each.value.email
-#   parent_id  = each.value.unit
+#   name     = each.key
+#   email    = each.value.email
+#   parent_id = each.value.unit
 # }
+
+variable "teams" {
+  description = "List of teams with their unit and organizational details"
+  type = list(object({
+    name     = string
+    unit     = string
+    accounts = list(string)
+  }))
+}
+
+variable "team_accounts" {
+  description = "Map of team accounts with names and email addresses"
+  type = map(object({
+    name  = string
+    email = string
+  }))
+}
+
+variable "teams_and_accounts" {
+  description = "List of teams and their account details"
+  type = list(object({
+    name     = string
+    unit     = string
+    accounts = list(string)
+  }))
+}
+
+variable "role_tags" {
+  description = "List of role tags used for differentiating environment roles"
+  type        = list(string)
+  default     = ["stg", "dev", "prod"]
+}
