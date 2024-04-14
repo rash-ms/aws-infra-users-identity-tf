e24e5143e0f8457a1f6c25846544cc0762a1839e
rash-ms <rashmujeeb@yahoo.com>
2024-04-14 18:04:17 +0100

    rename folder

--- tf-identity-setup/aws-orgz-team-unit/aws-orgz-team-unit.tf
+++ tf-identity-setup/aws-orgz-team-unit/aws-orgz-team-unit.tf
@@ -1,150 +1,44 @@
 data "aws_organizations_organization" "existing" {}
 
-resource "aws_organizations_organizational_unit" "team" {
-  for_each = var.teams
+variable "teams" {
+  type    = list(string)
+  default = ["data-team", "security-team", "marketing-team"]
+}
 
-  name      = each.key
+locals {
+  common_environments = ["Prod", "Non-prod"]
+  sub_environments    = ["dev", "stg"]
+
+  # Flatten into a list of maps
+  team_env_pairs = flatten([
+    for team in var.teams : [
+      for env in locals.common_environments : {
+        "team"        = team,
+        "environment" = env
+      }
+    ]
+  ])
+
+  # Convert the list of maps into a map for for_each
+  team_env_map = {
+    for pair in local.team_env_pairs :
+    "${pair.team}-${pair.environment}" => {
+      team        = pair.team,
+      environment = pair.environment
+    }
+  }
+}
+
+resource "aws_organizations_organizational_unit" "team" {
+  for_each = toset(var.teams)
+
+  name      = each.value
   parent_id = data.aws_organizations_organization.existing.roots[0].id
 }
 
-resource "aws_organizations_organizational_unit" "prod" {
-  for_each = var.teams
+resource "aws_organizations_organizational_unit" "environment" {
+  for_each = local.team_env_map
 
-  name      = "Prod"
-  parent_id = aws_organizations_organizational_unit.team[each.key].id
+  name      = each.value.environment
+  parent_id = aws_organizations_organizational_unit.team[each.value.team].id
 }
-
-resource "aws_organizations_organizational_unit" "non_prod" {
-  for_each = var.teams
-
-  name      = "Non-prod"
-  parent_id = aws_organizations_organizational_unit.team[each.key].id
-}
-
-resource "aws_organizations_organizational_unit" "prod_sub_ous" {
-  for_each = toset(flatten([
-    for team, detail in var.teams : [
-      for sub_ou in detail.prod_sub_ous : {
-        team_name = team
-        sub_ou_name = sub_ou
-      }
-    ]
-  ]))
-
-  name      = each.value.sub_ou_name
-  parent_id = aws_organizations_organizational_unit.prod[each.value.team_name].id
-}
-
-# Create sub-ous for Non-Prod environment for each team
-resource "aws_organizations_organizational_unit" "non_prod_sub_ous" {
-  for_each = toset(flatten([
-    for team, detail in var.teams : [
-      for sub_ou in detail.non_prod_sub_ous : {
-        team_name = team
-        sub_ou_name = sub_ou
-      }
-    ]
-  ]))
-
-  name      = each.value.sub_ou_name
-  parent_id = aws_organizations_organizational_unit.non_prod[each.value.team_name].id
-}
-
-
-
-# resource "aws_organizations_organizational_unit" "data_team" {
-#   name      = "data-team"
-#   parent_id = data.aws_organizations_organization.existing.roots[0].id
-# }
-
-# resource "aws_organizations_organizational_unit" "prod" {
-#   name      = "Prod"
-#   parent_id = aws_organizations_organizational_unit.data_team.id
-# }
-
-# resource "aws_organizations_organizational_unit" "non_prod" {
-#   name      = "Non-prod"
-#   parent_id = aws_organizations_organizational_unit.data_team.id
-# }
-
-# resource "aws_organizations_organizational_unit" "stg" {
-#   name      = "stg"
-#   parent_id = aws_organizations_organizational_unit.non_prod.id
-# }
-
-# resource "aws_organizations_organizational_unit" "dev" {
-#   name      = "dev"
-#   parent_id = aws_organizations_organizational_unit.non_prod.id
-# }
-
-# locals {
-#   all_teams = { for team in var.teams : team.name => team }
-# }
-
-# resource "aws_organizations_organizational_unit" "team" {
-#   for_each = locals.all_teams
-#   name     = each.key
-#   parent_id = aws_organizations_organizational_unit.non_prod.id
-# }
-
-# resource "aws_organizations_account" "team_accounts" {
-#   for_each = local.all_teams
-#   name     = "${each.key}-account"
-#   email    = var.team_accounts[each.key].email
-#   parent_id = aws_organizations_organizational_unit.team[each.key].id
-# }
-
-# data "external" "team_account_emails" {
-#   program = ["bash", "-c", "cat ${path.module}/team_account_emails.json"]
-# }
-
-# locals {
-#   accounts = flatten([
-#     for team in var.teams : [
-#       for account in team.accounts : {
-#         name  = account
-#         email = jsondecode(data.external.team_account_emails.result)[account]
-#         unit  = aws_organizations_organizational_unit.team[team.name].id
-#       }
-#     ]
-#   ])
-# }
-
-# resource "aws_organizations_account" "accounts" {
-#   for_each = { for account in local.accounts : account.name => account }
-#   name     = each.key
-#   email    = each.value.email
-#   parent_id = each.value.unit
-# }
-
-# variable "teams" {
-#   description = "List of teams with their unit and organizational details"
-#   type = list(object({
-#     name     = string
-#     unit     = string
-#     accounts = list(string)
-#   }))
-# }
-
-# variable "team_accounts" {
-#   description = "Map of team accounts with names and email addresses"
-#   type = map(object({
-#     name  = string
-#     email = string
-#   }))
-# }
-
-# variable "teams_and_accounts" {
-#   description = "List of teams and their account details"
-#   type = list(object({
-#     name     = string
-#     unit     = string
-#     accounts = list(string)
-#   }))
-# }
-
-# variable "role_tags" {
-#   description = "List of role tags used for differentiating environment roles"
-#   type        = list(string)
-#   default     = ["stg", "dev", "prod"]
-# }
