93a21f84ad5e9ac5f98249c9cdef3c8792a96094
rash-ms <rashmujeeb@yahoo.com>
2024-04-14 20:16:36 +0100

    modify code to create aws orgz

--- tf-identity-setup/aws-orgz-team-unit/aws-orgz-team-unit.tf
+++ tf-identity-setup/aws-orgz-team-unit/aws-orgz-team-unit.tf
@@ -12,7 +12,7 @@ locals {
     ]
   ])
 
-  # Create a list of maps for each team-environment-sub_environment trio where the environment is Non-prod
+  # Create a list of maps for each team-environment-sub_environment 
   team_sub_env_pairs = flatten([
     for team in var.teams : [
       for sub_env in var.sub_environments: {
@@ -25,7 +25,7 @@ locals {
   
   # Convert the list of maps into a map for for_each for the environments
   team_env_map = {
-    for pair in local.team_env_pairs :  # Removed 'locals.' prefix
+    for pair in local.team_env_pairs :  
     "${pair.team}-${pair.environment}" => {
       team        = pair.team,
       environment = pair.environment
@@ -64,3 +64,46 @@ resource "aws_organizations_organizational_unit" "sub_environment" {
   name      = each.value.sub_environment
   parent_id = aws_organizations_organizational_unit.environment["${each.value.team}-Non-prod"].id
 }
+
+
+
+# locals {
+#   all_teams = { for team in var.teams : team.name => team }
+# }
+
+# resource "aws_organizations_organizational_unit" "team" {
+#   for_each = locals.all_teams
+#   name     = each.key
+#   parent_id = aws_organizations_organizational_unit.non_prod.id
+# }
+
+# resource "aws_organizations_account" "team_accounts" {
+#   for_each = local.all_teams
+#   name     = "${each.key}-account"
+#   email    = var.team_accounts[each.key].email
+#   parent_id = aws_organizations_organizational_unit.team[each.key].id
+# }
+
+# data "external" "team_account_emails" {
+#   program = ["bash", "-c", "cat ${path.module}/team_account_emails.json"]
+# }
+
+# locals {
+#   accounts = flatten([
+#     for team in var.teams : [
+#       for account in team.accounts : {
+#         name  = account
+#         email = jsondecode(data.external.team_account_emails.result)[account]
+#         unit  = aws_organizations_organizational_unit.team[team.name].id
+#       }
+#     ]
+#   ])
+# }
+
+# resource "aws_organizations_account" "accounts" {
+#   for_each = { for account in local.accounts : account.name => account }
+#   name     = each.key
+#   email    = each.value.email
+#   parent_id = each.value.unit
+# }
+
--- tf-identity-setup/tf-identity-deployment/aws-team-organization.tf
+++ tf-identity-setup/tf-identity-deployment/aws-team-organization.tf
@@ -1,22 +1,14 @@
-# module "aws-team-organization" {
-#   source             = "../aws-orgz-team-unit/"
-#   teams              = var.teams
-
-#   # role_tags          = var.role_tags
-#   # team_accounts      = var.team_accounts
-#   # teams_and_accounts = var.teams_and_accounts
-
-#   providers = {
-#     aws = aws.aws-us-east-1
-#   }
-# }
-
-
 module "aws-team-organization" {
   source              = "../aws-orgz-team-unit"
   teams               = var.teams 
   common_environments = var.common_environments
   sub_environments    = var.sub_environments
+
+  #   teams              = var.teams
+
+  # role_tags          = var.role_tags
+  # team_accounts      = var.team_accounts
+  # teams_and_accounts = var.teams_and_accounts
 
   providers = {
     aws = aws.aws-us-east-1
