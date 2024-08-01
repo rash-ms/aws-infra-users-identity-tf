b1b2f581a5ad9c1793b6c592a0dd4c806dc0777c
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-01 15:32:48 +0100

    refactor the aws org unit code

--- aws-tf-identity-setup/aws-orgz-team-unit/variables.tf
+++ aws-tf-identity-setup/aws-orgz-team-unit/variables.tf
@@ -1,15 +1,3 @@
-# variable "teams" {
-#   description = "List of team name"
-#   type    = list(string)
-#   default = ["data-eng-solution", "infra-team", "marketing-team"]
-# }
-
-# variable "workspace" {
-#   description = "List of workspace for each team."
-#   type        = list(string)
-#   default     = ["Prod", "Non-prod"]
-# }
-
 variable "teams" {
   description = "List of teams"
   type        = list(string)
@@ -21,12 +9,3 @@ variable "workspace" {
   type        = list(string)
   default     = ["Prod", "NonProd"]
 }
-
-# variable "team_account_emails" {
-#   description = "Map of team and environment to account email"
-#   type        = map(string)
-#   default = {
-#     "dataorg-Prod" = "aws-bdt-dataorg-prod@bagitek.com"
-#     "dataorg-NonProd" = "aws-bdt-dataorg-nonprod@bagitek.com"
-#   }
-# }
