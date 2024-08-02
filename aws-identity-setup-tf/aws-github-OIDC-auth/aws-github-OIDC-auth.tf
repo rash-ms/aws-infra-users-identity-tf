62c1085c31cb2b850ffa360da79f9ee32d810984
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-02 21:25:58 +0100

    create group iam for user

--- aws-identity-setup-tf/aws-github-OIDC-auth/aws-github-OIDC-auth.tf
+++ aws-identity-setup-tf/aws-github-OIDC-auth/aws-github-OIDC-auth.tf
@@ -1,15 +1,73 @@
+# resource "aws_iam_openid_connect_provider" "github_oidc" {
+#     client_id_list  =   ["sts.amazonaws.com"]
+#     thumbprint_list =   ["1b511abead59c6ce207077c0bf0e0043b1382612"]
+#     url             =   "https://token.actions.githubusercontent.com"
+# }
+
+# resource "aws_iam_role" "github_actions" {
+#   name               = var.github-action-name
+#   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+#   tags               = var.github-action-role-tags
+# }
+
+# resource "aws_iam_role_policy_attachment" "github_actions_admin_access_attach" {
+#   role       = aws_iam_role.github_actions.name
+#   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+# }
+
+# data "aws_iam_policy_document" "assume_role_policy" {
+#     statement {
+#       actions = ["sts:AssumeRoleWithWebIdentity"]
+#       effect  = "Allow"
+#       principals {
+#         type        =  "Federated"
+#         identifiers = [aws_iam_openid_connect_provider.github_oidc.arn]
+#       }
+#       condition {
+#         test     = "StringEquals"
+#         variable = "token.actions.githubusercontent.com:aud"
+#         values   = ["sts.amazonaws.com"]
+#       }
+#       condition {
+#         test     = "StringLike"
+#         variable = "token.actions.githubusercontent.com:sub"
+#         values   = ["repo:rash-ms/*"]
+
+#       }
+#     }
+# }
+
+
+#######################################################
+
+provider "aws" {
+  alias  = "dev_account"
+  region = "us-east-1"
+  profile = "BDT-data-org-DEV" 
+}
+
+provider "aws" {
+  alias  = "prod_account"
+  region = "us-east-1"
+  profile = "BDT-data-org-PROD" 
+}
+
 locals {
 
   policies_data = jsondecode(file("${path.module}/../aws-orgz-team-unit/policies.json"))
+  policies      = local.policies_data.policies
   groups        = local.policies_data.groups
 
-  policies = {
-    "data-eng-DEV"  = "arn:aws:iam::021891586814:policy/data-eng-DEV-fullAccess",
-    "data-eng-PROD" = "arn:aws:iam::021891586728:policy/data-eng-PROD-readonly"
+  account_ids = {
+    "data-eng-DEV"  = "021891586814"  
+    "data-eng-PROD" = "021891586728"  
   }
 }
 
 resource "aws_iam_openid_connect_provider" "github_oidc" {
+  for_each = local.account_ids
+  provider = each.key == "BDT-data-org-DEV" ? aws.dev_account : aws.prod_account
+
   client_id_list = ["sts.amazonaws.com"]
   url = "https://token.actions.githubusercontent.com"
   thumbprint_list = ["1b511abead59c6ce207077c0bf0e0043b1382612"]
@@ -18,6 +76,7 @@ resource "aws_iam_openid_connect_provider" "github_oidc" {
 
 resource "aws_iam_role" "roles" {
   for_each = local.groups
+  provider = each.key == "BDT-data-org-DEV" ? aws.dev_account : aws.prod_account
 
   name = "${each.key}_role"
   
@@ -39,8 +98,20 @@ resource "aws_iam_role" "roles" {
   })
 }
 
+resource "aws_iam_policy" "policies" {
+  for_each = local.policies
+
+  provider = each.key == "BDT-data-org-DEV" ? aws.dev_account : aws.prod_account
+
+  name        = each.value.name
+  description = each.value.description
+  policy      = jsonencode(each.value.policy)
+}
+
 resource "aws_iam_role_policy_attachment" "policy_attachment" {
   for_each = local.groups
+
+  provider = each.key == "BDT-data-org-DEV" ? aws.dev_account : aws.prod_account
 
   role       = aws_iam_role.roles[each.key].name
   policy_arn = local.policies[each.key]
