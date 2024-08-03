62f45fdb17762a03eb0a769bf770e465005f0540
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-03 22:45:01 +0100

    test permset

--- aws-identity-setup-tf/aws-github-OIDC-auth/aws-github-OIDC-auth.tf
+++ aws-identity-setup-tf/aws-github-OIDC-auth/aws-github-OIDC-auth.tf
@@ -1,73 +1,19 @@
-# resource "aws_iam_openid_connect_provider" "github_oidc" {
-#     client_id_list  =   ["sts.amazonaws.com"]
-#     thumbprint_list =   ["1b511abead59c6ce207077c0bf0e0043b1382612"]
-#     url             =   "https://token.actions.githubusercontent.com"
+# provider "aws" {
+#   alias  = "data_eng_dev"
+#   region = "us-east-1"
+#   assume_role {
+#     role_arn = "arn:aws:iam::021891586814:role/bdt-data-org-dev-role"
+#   }
 # }
 
-# resource "aws_iam_role" "github_actions" {
-#   name               = var.github-action-name
-#   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
-#   tags               = var.github-action-role-tags
+# provider "aws" {
+#   alias  = "data_eng_prod"
+#   region = "us-east-1"
+#   assume_role {
+#     role_arn = "arn:aws:iam::021891586728:role/bdt-data-org-prod-role"
+#   }
 # }
 
-# resource "aws_iam_role_policy_attachment" "github_actions_admin_access_attach" {
-#   role       = aws_iam_role.github_actions.name
-#   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-# }
-
-# data "aws_iam_policy_document" "assume_role_policy" {
-#     statement {
-#       actions = ["sts:AssumeRoleWithWebIdentity"]
-#       effect  = "Allow"
-#       principals {
-#         type        =  "Federated"
-#         identifiers = [aws_iam_openid_connect_provider.github_oidc.arn]
-#       }
-#       condition {
-#         test     = "StringEquals"
-#         variable = "token.actions.githubusercontent.com:aud"
-#         values   = ["sts.amazonaws.com"]
-#       }
-#       condition {
-#         test     = "StringLike"
-#         variable = "token.actions.githubusercontent.com:sub"
-#         values   = ["repo:rash-ms/*"]
-
-#       }
-#     }
-# }
-
-
-#######################################################
-
-provider "aws" {
-  alias  = "data_eng_dev"
-  region = "us-east-1"
-  assume_role {
-    role_arn = "arn:aws:iam::021891586814:user/admin"
-  }
-}
-
-provider "aws" {
-  alias  = "data_eng_prod"
-  region = "us-east-1"
-  assume_role {
-    role_arn = "arn:aws:iam::021891586728:user/admin-prod"
-  }
-}
-
-
-locals {
-
-  policies_data = jsondecode(file("${path.module}/../aws-orgz-team-unit/policies.json"))
-  policies      = local.policies_data.policies
-  groups        = local.policies_data.groups
-
-  account_ids = {
-    "data-eng-DEV"  = "021891586814"  
-    "data-eng-PROD" = "021891586728"  
-  }
-}
 
 resource "aws_iam_openid_connect_provider" "github_oidc_dev" {
   provider = aws.data_eng_dev
@@ -78,7 +24,7 @@ resource "aws_iam_openid_connect_provider" "github_oidc_dev" {
 }
 
 resource "aws_iam_openid_connect_provider" "github_oidc_prod" {
-  provider = aws.data_eng_prod
+#   provider = aws.data_eng_prod
 
   client_id_list  = ["sts.amazonaws.com"]
   url             = "https://token.actions.githubusercontent.com"
@@ -86,11 +32,9 @@ resource "aws_iam_openid_connect_provider" "github_oidc_prod" {
 }
 
 resource "aws_iam_role" "roles_dev" {
-  for_each = local.groups
+#   provider = aws.data_eng_dev
 
-  provider = aws.data_eng_dev
-
-  name = "${each.key}_role"
+  name = "dev-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -111,11 +55,9 @@ resource "aws_iam_role" "roles_dev" {
 }
 
 resource "aws_iam_role" "roles_prod" {
-  for_each = local.groups
+#   provider = aws.data_eng_prod
 
-  provider = aws.data_eng_prod
-
-  name = "${each.key}_role"
+  name = "prod-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -135,40 +77,18 @@ resource "aws_iam_role" "roles_prod" {
   })
 }
 
-resource "aws_iam_policy" "policies_dev" {
-  for_each = local.policies
-
-  provider = aws.data_eng_dev
-
-  name        = "${each.key}-policy"
-  description = "Policy for ${each.key}"
-  policy      = jsonencode(each.value.full_access_policy)
-}
-
-resource "aws_iam_policy" "policies_prod" {
-  for_each = local.policies
-
-  provider = aws.data_eng_prod
-
-  name        = "${each.key}-policy"
-  description = "Policy for ${each.key}"
-  policy      = jsonencode(each.value.readonly_policy)
-}
-
 resource "aws_iam_role_policy_attachment" "policy_attachment_dev" {
-  for_each = local.groups
+#   provider = aws.data_eng_dev
 
-  provider = aws.data_eng_dev
-
-  role       = aws_iam_role.roles_dev[each.key].name
-  policy_arn = aws_iam_policy.policies_dev[each.key].arn
+  role       = aws_iam_role.roles_dev.name
+#   policy_arn = "arn:aws:iam::0219475372814:policy/dev-policy"
+  policy_arn = "arn:aws:iam::021891586814:policy/bdt-data-org-dev-role-policy"
 }
 
 resource "aws_iam_role_policy_attachment" "policy_attachment_prod" {
-  for_each = local.groups
+#   provider = aws.data_eng_prod
 
-  provider = aws.data_eng_prod
-
-  role       = aws_iam_role.roles_prod[each.key].name
-  policy_arn = aws_iam_policy.policies_prod[each.key].arn
+  role       = aws_iam_role.roles_prod.name
+#   policy_arn = "arn:aws:iam::021464739328:policy/prod-policy"
+  policy_arn = "arn:aws:iam::021891586728:policy/bdt-data-org-prod-role-policy"
 }
