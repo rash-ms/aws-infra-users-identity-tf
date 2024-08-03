cc5548a06d13d48fed94a44f941b740ec8b0a40e
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-03 21:29:47 +0100

    testing perm set

--- aws-identity-deployment-tf/aws-github-OIDC-auth.tf
+++ aws-identity-deployment-tf/aws-github-OIDC-auth.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_version = ">=v0.14.7"
+  backend "s3" {
+    bucket         = "bdt-infra-resource-backend"
+    key            = "terraform/complete-state"
+    region         = "us-east-1"                
+  }
+}
+
 module "aws-github-OIDC-auth" {
   source             = "../aws-identity-setup-tf/aws-github-OIDC-auth"
   github_action_name    = var.github_action_name
--- aws-identity-deployment-tf/providers.tf
+++ aws-identity-deployment-tf/providers.tf
@@ -3,11 +3,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform {
-  required_version = ">=v0.14.7"
-  backend "s3" {
-    bucket         = "byt-infra-users-identity-backend"
-    key            = "terraform/complete-state"
-    region         = "us-east-1"                
-  }
-}
+# terraform {
+#   required_version = ">=v0.14.7"
+#   backend "s3" {
+#     bucket         = "byt-infra-users-identity-backend"
+#     key            = "terraform/complete-state"
+#     region         = "us-east-1"                
+#   }
+# }
