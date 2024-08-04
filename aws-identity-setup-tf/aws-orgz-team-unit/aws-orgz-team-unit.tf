122cd812b84e04a7edce4bb097acfde7e82587f3
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-04 19:23:02 +0100

    test permset

--- aws-identity-setup-tf/aws-orgz-team-unit/aws-orgz-team-unit.tf
+++ aws-identity-setup-tf/aws-orgz-team-unit/aws-orgz-team-unit.tf
@@ -39,19 +39,6 @@ locals {
   }
 }
 
-
-variable "teams" {
-  description = "List of teams"
-  type        = list(string)
-  default     = ["data-org"]
-}
-
-variable "workspace" {
-  description = "List of workspaces"
-  type        = list(string)
-  default     = ["PROD", "DEV"]
-}
-
 data "aws_organizations_organization" "existing" {}
 
 data "aws_organizations_organizational_units" "existing_ous" {
