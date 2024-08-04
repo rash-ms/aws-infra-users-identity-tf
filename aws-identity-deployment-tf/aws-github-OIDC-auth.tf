47369f51f9660e63364df194912f1e71ae30c435
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-04 13:56:35 +0100

    test permset

--- aws-identity-deployment-tf/aws-github-OIDC-auth.tf
+++ aws-identity-deployment-tf/aws-github-OIDC-auth.tf
@@ -7,3 +7,19 @@ module "aws-github-OIDC-auth" {
     aws = aws.aws-us-east-1
   }
 }
+
+variable "github_action_name" {
+  description = "The name for the GitHub Actions IAM role"
+  type        = string
+  default     = "GitHubAction-AssumeRole"
+}
+
+variable "github_action_role_tags" {
+  description = "A map of tags to assign to the role"
+  type        = map(string)
+  default     = {
+    RoleWorkspace-0 = "stg"
+    RoleWorkspace-1 = "dev"
+    RoleWorkspace-2 = "prod"
+  }
+}
--- aws-identity-deployment-tf/aws-team-orgz-unit.tf
+++ aws-identity-deployment-tf/aws-team-orgz-unit.tf
@@ -1,9 +1,22 @@
-# module "aws-team-orgz-unit" {
-#   source              = "../aws-identity-setup-tf/aws-orgz-team-unit"
-#   teams               = var.teams 
-#   workspace           = var.workspace
+module "aws-team-orgz-unit" {
+  source              = "../aws-identity-setup-tf/aws-orgz-team-unit"
+  teams               = var.teams 
+  workspace           = var.workspace
 
-#   providers = {
-#     aws = aws.aws-us-east-1
-#   }
-# }
+  providers = {
+    aws = aws.aws-us-east-1
+  }
+}
+
+
+variable "teams" {
+  description = "List of teams"
+  type        = list(string)
+  default     = ["data-org"]
+}
+
+variable "workspace" {
+  description = "List of workspaces"
+  type        = list(string)
+  default     = ["PROD", "DEV"]
+}
