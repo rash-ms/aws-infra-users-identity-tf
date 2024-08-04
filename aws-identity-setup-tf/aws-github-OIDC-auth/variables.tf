e632f9dde49b01a8f2a5aa5e6a6be97e20234d6f
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-05 00:51:51 +0100

    creating oidc connection

--- aws-identity-setup-tf/aws-github-OIDC-auth/variables.tf
+++ aws-identity-setup-tf/aws-github-OIDC-auth/variables.tf
@@ -1,15 +1,41 @@
-variable "github_action_name" {
-  description = "The name for the GitHub Actions IAM role"
-  type        = string
-  default     = "GitHubAction-AssumeRole"
+variable "workspaces" {
+  description = "List of workspaces"
+  type        = list(string)
+  default     = ["dev", "prod"]
 }
 
-variable "github_action_role_tags" {
-  description = "A map of tags to assign to the role"
+variable "role_arns" {
+  description = "Map of workspace to role ARN"
   type        = map(string)
   default     = {
-    RoleWorkspace-0 = "stg"
-    RoleWorkspace-1 = "dev"
-    RoleWorkspace-2 = "prod"
+    dev  = "arn:aws:iam::022499035350:role/byt-data-org-dev-role",
+    prod = "arn:aws:iam::022499035568:role/byt-data-org-prod-role"
   }
 }
+
+variable "policy_arns" {
+  description = "Map of workspace to policy ARN"
+  type        = map(string)
+  default     = {
+    dev  = "arn:aws:iam::022499035350:policy/byt-data-org-dev-policy",
+    prod = "arn:aws:iam::022499035568:policy/byt-data-org-prod-policy"
+  }
+}
+
+
+# variable "github_action_name" {
+#   description = "The name for the GitHub Actions IAM role"
+#   type        = string
+#   default     = "GitHubAction-AssumeRole"
+# }
+
+# variable "github_action_role_tags" {
+#   description = "A map of tags to assign to the role"
+#   type        = map(string)
+#   default     = {
+#     RoleWorkspace-0 = "stg"
+#     RoleWorkspace-1 = "dev"
+#     RoleWorkspace-2 = "prod"
+#   }
+# }
+
