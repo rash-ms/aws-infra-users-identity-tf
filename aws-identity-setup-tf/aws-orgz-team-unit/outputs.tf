0458bd5edb4254455991d1294a9813b585c939b8
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-04 22:53:17 +0100

    creating aws acct in OU

--- aws-identity-setup-tf/aws-orgz-team-unit/outputs.tf
+++ aws-identity-setup-tf/aws-orgz-team-unit/outputs.tf
@@ -2,9 +2,9 @@
 #   value = aws_organizations_organization.org.id
 # }
 
-# output "team_env_account_ids" {
-#   value = { for k, v in aws_organizations_account.team_env_account : k => v.id }
-# }
+output "team_wrkspc_account_ids" {
+  value = { for k, v in aws_organizations_account.team_wrkspc_account : k => v.id }
+}
 
 
 output "account_map" {
