52625b74af04352f8eb0cbfcd877cf40e67e9d8c
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-04 10:58:11 +0100

    test permset

--- aws-identity-setup-tf/aws-github-OIDC-auth/outputs.tf
+++ aws-identity-setup-tf/aws-github-OIDC-auth/outputs.tf
@@ -1,7 +1,7 @@
 output "iam_roles_dev" {
-  value = aws_iam_role.roles_dev
+  value = aws_iam_role.roles_byt_dev
 }
 
 output "iam_roles_prod" {
-  value = aws_iam_role.roles_prod
+  value = aws_iam_role.roles_byt_prod
 }
