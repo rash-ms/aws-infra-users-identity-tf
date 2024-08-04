527a464d8a45cdc1ce9d30811601d2691600a0c1
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-05 00:27:37 +0100

    creating aws acct in OU

--- aws-identity-deployment-tf/aws-github-OIDC-auth.tf
+++ aws-identity-deployment-tf/aws-github-OIDC-auth.tf
@@ -1,25 +1,10 @@
-# module "aws-github-OIDC-auth" {
-#   source             = "../aws-identity-setup-tf/aws-github-OIDC-auth"
-#   github_action_name    = var.github_action_name
-#   github_action_role_tags = var.github_action_role_tags
+module "aws-github-OIDC-auth" {
+  source             = "../aws-identity-setup-tf/aws-github-OIDC-auth"
+  github_action_name    = var.github_action_name
+  github_action_role_tags = var.github_action_role_tags
 
-#   providers = {
-#     aws = aws.aws-us-east-1
-#   }
-# }
+  providers = {
+    aws = aws.aws-us-east-1
+  }
+}
 
-# variable "github_action_name" {
-#   description = "The name for the GitHub Actions IAM role"
-#   type        = string
-#   default     = "GitHubAction-AssumeRole"
-# }
-
-# variable "github_action_role_tags" {
-#   description = "A map of tags to assign to the role"
-#   type        = map(string)
-#   default     = {
-#     RoleWorkspace-0 = "stg"
-#     RoleWorkspace-1 = "dev"
-#     RoleWorkspace-2 = "prod"
-#   }
-# }
