a43d99b402ca359a9a4f75fc4515cfab0db2232e
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-05 01:16:38 +0100

    creating oidc connection

--- aws-identity-setup-tf/aws-github-OIDC-auth/variables.tf
+++ aws-identity-setup-tf/aws-github-OIDC-auth/variables.tf
@@ -1,16 +1,16 @@
-# variable "github_action_name" {
-#   description = "The name for the GitHub Actions IAM role"
-#   type        = string
-#   default     = "GitHubAction-AssumeRole"
-# }
+variable "github_action_name" {
+  description = "The name for the GitHub Actions IAM role"
+  type        = string
+  default     = "GitHubAction-AssumeRole"
+}
 
-# variable "github_action_role_tags" {
-#   description = "A map of tags to assign to the role"
-#   type        = map(string)
-#   default     = {
-#     RoleWorkspace-0 = "stg"
-#     RoleWorkspace-1 = "dev"
-#     RoleWorkspace-2 = "prod"
-#   }
-# }
+variable "github_action_role_tags" {
+  description = "A map of tags to assign to the role"
+  type        = map(string)
+  default     = {
+    RoleWorkspace-0 = "stg"
+    RoleWorkspace-1 = "dev"
+    RoleWorkspace-2 = "prod"
+  }
+}
 
