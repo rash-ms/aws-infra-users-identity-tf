a205477de46685a61a2b010edebce895708d9947
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-05 23:19:56 +0100

    create aws sso for user

--- aws-identity-deployment-tf/aws-users-identity.tf
+++ aws-identity-deployment-tf/aws-users-identity.tf
@@ -1,9 +1,21 @@
+# module "prod_users" {
+#   source    = "../aws-identity-setup-tf/aws-users-identity-creation"
+# #   yaml_path = "${path.module}/../base_conf/byt-aws-prod.yaml"
+#   yaml_path = "${path.module}/aws-users-identity-creation/base_conf/byt-aws-prod.yaml"
+# }
+
+# module "dev_users" {
+#   source    = "../aws-identity-setup-tf/aws-users-identity-creation"
+#   yaml_path = "${path.module}/../base_conf/byt-aws-dev.yaml"
+#    yaml_path = "${path.module}/aws-users-identity-creation/base_conf/byt-aws-prod.yaml"
+# }
+
 module "prod_users" {
-  source    = "../aws-identity-setup-tf/aws-users-identity-creation"
-  yaml_path = "${path.module}/base_conf/byt-aws-prod.yaml"
+  source    = "./aws-users-identity-creation"
+  yaml_path = "${path.module}/aws-users-identity-creation/base_conf/byt-aws-prod.yaml"
 }
 
 module "dev_users" {
-  source    = "../aws-identity-setup-tf/aws-users-identity-creation"
-  yaml_path = "${path.module}/base_conf/byt-aws-dev.yaml"
+  source    = "./aws-users-identity-creation"
+  yaml_path = "${path.module}/aws-users-identity-creation/base_conf/byt-aws-dev.yaml"
 }
