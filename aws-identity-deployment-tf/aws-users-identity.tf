d259fb7c71e4107d8efa9621271a90c702b7539c
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-06 00:55:46 +0100

    create aws sso for user

--- aws-identity-deployment-tf/aws-users-identity.tf
+++ aws-identity-deployment-tf/aws-users-identity.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 module "prod_users" {
   source    = "../aws-identity-setup-tf/aws-users-identity-creation"
-  yaml_path = "../aws-identity-setup-tf/${path.module}/base_conf/byt-aws-prod.yaml"
+  yaml_path = "../aws-identity-setup-tf/${path.module}/../base_conf/byt-aws-prod.yaml"
 }
 
 module "dev_users" {
   source    = "../aws-identity-setup-tf/aws-users-identity-creation"
-  yaml_path = "../aws-identity-setup-tf/${path.module}/base_conf/byt-aws-dev.yaml"
+  yaml_path = "../aws-identity-setup-tf/${path.module}/../base_conf/byt-aws-dev.yaml"
 }
