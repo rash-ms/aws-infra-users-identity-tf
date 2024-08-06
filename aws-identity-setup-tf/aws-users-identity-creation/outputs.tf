e79ad0a490a0f3cf08e59ccbeaf067fdd6766944
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-06 11:53:11 +0100

    create aws sso for user

--- aws-identity-setup-tf/aws-users-identity-creation/outputs.tf
+++ aws-identity-setup-tf/aws-users-identity-creation/outputs.tf
@@ -1,8 +1,3 @@
-# output "user_ids" {
-#   value = local.user_ids
-# }
-
-# # Remove the duplicate user_ids output if it exists here
-# output "debug_mappings" {
-#   value = local.flattened_user_groups
-# }
+output "debug_mappings" {
+  value = local.flattened_user_groups
+}
