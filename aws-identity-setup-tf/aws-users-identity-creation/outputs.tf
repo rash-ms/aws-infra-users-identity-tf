31e4140cc8f0dfd50e9e6ebf9a0371dfd1b2763e
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-06 23:51:04 +0100

    create aws sso for user

--- aws-identity-setup-tf/aws-users-identity-creation/outputs.tf
+++ aws-identity-setup-tf/aws-users-identity-creation/outputs.tf
@@ -1,9 +1,7 @@
-# output "existing_users" {
-#   description = "Existing users in the identity store"
-#   value       = aws_identitystore_user.users
-# }
+output "created_users" {
+  value = aws_identitystore_user.users
+}
 
-# output "existing_groups" {
-#   description = "Existing groups in the identity store"
-#   value       = { for k, v in data.aws_identitystore_group.existing_groups : k => try(v.id, "Group not found") }
-# }
+output "group_memberships" {
+  value = aws_identitystore_group_membership.memberships
+}
