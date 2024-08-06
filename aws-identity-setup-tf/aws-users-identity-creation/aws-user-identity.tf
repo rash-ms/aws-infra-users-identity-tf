aceecc568b6a976d8ff6ea0a60f888dad08285f9
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-06 01:03:40 +0100

    create aws sso for user

--- aws-identity-setup-tf/aws-users-identity-creation/aws-user-identity.tf
+++ aws-identity-setup-tf/aws-users-identity-creation/aws-user-identity.tf
@@ -1,12 +1,17 @@
 locals {
   config = yamldecode(file(var.yaml_path))
 
-  # Flatten the user_groups into a single map
+  group_to_permission_set = {
+    "byt-data-eng-fullAccess" = "byt-data-eng-DEV-fullAccess"
+    "byt-data-eng-readonly" = "byt-data-eng-PROD-readonly"
+  }
+
   flattened_user_groups = merge([
     for group_name, users in local.config : {
       for user in users : "${group_name}-${user}" => {
         group = group_name
         user  = user
+        permission_set = local.group_to_permission_set[group_name]
       }
     }
   ]...)
@@ -16,6 +21,7 @@ data "aws_ssoadmin_instances" "main" {}
 
 data "aws_organizations_organization" "main" {}
 
+# Identity Store Users
 data "aws_identitystore_user" "sso_users" {
   for_each          = local.flattened_user_groups
   identity_store_id = data.aws_ssoadmin_instances.main.identity_store_ids[0]
@@ -28,17 +34,18 @@ data "aws_identitystore_user" "sso_users" {
   }
 }
 
+# Permission Sets
 data "aws_ssoadmin_permission_set" "all" {
   for_each     = toset([for group, _ in local.config : group])
   instance_arn = data.aws_ssoadmin_instances.main.arns[0]
-  name         = each.key
+  name         = local.group_to_permission_set[each.key]
 }
 
 resource "aws_ssoadmin_account_assignment" "assignments" {
   for_each = local.flattened_user_groups
 
   instance_arn       = data.aws_ssoadmin_instances.main.arns[0]
-  permission_set_arn = data.aws_ssoadmin_permission_set.all[each.value.group].arn
+  permission_set_arn = data.aws_ssoadmin_permission_set.all[each.value.permission_set].arn
   principal_type     = "USER"
   target_id          = data.aws_organizations_organization.main.id
   target_type        = "AWS_ACCOUNT"
