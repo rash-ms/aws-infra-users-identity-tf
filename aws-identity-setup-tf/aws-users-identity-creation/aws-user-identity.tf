69d66473399f08f3d04c1fc64d0d4add49483c6b
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-06 02:05:03 +0100

    create aws sso for user

--- aws-identity-setup-tf/aws-users-identity-creation/aws-user-identity.tf
+++ aws-identity-setup-tf/aws-users-identity-creation/aws-user-identity.tf
@@ -34,19 +34,31 @@ resource "null_resource" "get_user_ids" {
 
   provisioner "local-exec" {
     command = <<EOT
-      user_id=$(aws identitystore list-users --identity-store-id ${data.aws_ssoadmin_instances.main.identity_store_ids[0]} --query "Users[?UserName=='${each.value.user}'].UserId" --output text)
-      echo ${user_id} > ${path.module}/user_id_${each.value.user}.txt
+      aws identitystore list-users --identity-store-id ${data.aws_ssoadmin_instances.main.identity_store_ids[0]} --query "Users[?UserName=='${each.value.user}'].UserId" --output text > user_id_${each.value.user}.txt
     EOT
   }
 }
 
-resource "null_resource" "add_users_to_group" {
+data "local_file" "user_ids" {
   for_each = { for user_map in local.flattened_user_groups : user_map.user => user_map }
+  filename = "user_id_${each.key}.txt"
   depends_on = [null_resource.get_user_ids]
+}
+
+locals {
+  user_ids = {
+    for user in local.flattened_user_groups : 
+    user.user => chomp(data.local_file.user_ids[user.user].content)
+  }
+}
+
+resource "null_resource" "add_users_to_group" {
+  for_each = local.user_ids
+  depends_on = [data.local_file.user_ids]
 
   provisioner "local-exec" {
     command = <<EOT
-      aws identitystore create-group-membership --identity-store-id ${data.aws_ssoadmin_instances.main.identity_store_ids[0]} --group-id ${lookup(local.config, each.value.group, null)} --member-id $(cat ${path.module}/user_id_${each.value.user}.txt)
+      aws identitystore create-group-membership --identity-store-id ${data.aws_ssoadmin_instances.main.identity_store_ids[0]} --group-id ${lookup(local.config, each.value.group, null)} --member-id ${each.value}
     EOT
   }
 }
@@ -54,4 +66,8 @@ resource "null_resource" "add_users_to_group" {
 # Debug output to verify the mappings
 output "debug_mappings" {
   value = local.flattened_user_groups
+}
+
+output "user_ids" {
+  value = local.user_ids
 }
--- aws-identity-setup-tf/aws-users-identity-creation/outputs.tf
+++ aws-identity-setup-tf/aws-users-identity-creation/outputs.tf
@@ -2,6 +2,9 @@
 #   value = [for user in aws_iam_identity_center_user.users : user.id]
 # }
 
+# output "user_ids" {
+#   value = [for user in null_resource.create_users : user.id]
+# }
 output "user_ids" {
-  value = [for user in null_resource.create_users : user.id]
+  value = local.user_ids
 }
