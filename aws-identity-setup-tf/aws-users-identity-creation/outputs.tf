a83d384e29ba843e93fd45279b90755ed772ccc6
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-06 02:06:19 +0100

    create aws sso for user

--- aws-identity-setup-tf/aws-users-identity-creation/outputs.tf
+++ aws-identity-setup-tf/aws-users-identity-creation/outputs.tf
@@ -1,10 +1,3 @@
-# output "user_ids" {
-#   value = [for user in aws_iam_identity_center_user.users : user.id]
-# }
-
-# output "user_ids" {
-#   value = [for user in null_resource.create_users : user.id]
-# }
 output "user_ids" {
   value = local.user_ids
 }
