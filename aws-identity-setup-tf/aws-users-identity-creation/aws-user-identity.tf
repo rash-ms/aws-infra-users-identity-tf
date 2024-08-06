d4a005c612497e61b22ec888724d49d5ef38bb6c
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-06 01:29:20 +0100

    create aws sso for user

--- aws-identity-setup-tf/aws-users-identity-creation/aws-user-identity.tf
+++ aws-identity-setup-tf/aws-users-identity-creation/aws-user-identity.tf
@@ -1,15 +1,22 @@
 locals {
   config = yamldecode(file(var.yaml_path))
 
-  # Flatten the user_groups into a single map
-  flattened_user_groups = merge([
-    for group_name, users in local.config : {
-      for user in users : "${group_name}-${user}" => {
+  # Mapping of groups to permission sets
+  group_to_permission_set = {
+    "byt-data-eng-fullAccess" = "byt-data-eng-fullAccess"
+    "byt-data-eng-readonly" = "byt-data-eng-readonly"
+  }
+
+  # Flatten the user_groups into a list of maps
+  flattened_user_groups = [
+    for group_name, users in local.config : [
+      for user in users : {
         group = group_name
         user  = user
+        permission_set = lookup(local.group_to_permission_set, group_name, null)
       }
-    }
-  ]...)
+    ]
+  ]
 }
 
 data "aws_ssoadmin_instances" "main" {}
@@ -17,7 +24,7 @@ data "aws_ssoadmin_instances" "main" {}
 data "aws_organizations_organization" "main" {}
 
 resource "null_resource" "create_users" {
-  for_each = local.flattened_user_groups
+  for_each = { for user_map in flatten(local.flattened_user_groups) : user_map.user => user_map }
 
   provisioner "local-exec" {
     command = <<EOT
@@ -27,23 +34,36 @@ resource "null_resource" "create_users" {
 }
 
 resource "null_resource" "get_user_ids" {
-  for_each = local.flattened_user_groups
+  for_each = { for user_map in flatten(local.flattened_user_groups) : user_map.user => user_map }
   depends_on = [null_resource.create_users]
 
   provisioner "local-exec" {
     command = <<EOT
-      aws identitystore list-users --identity-store-id ${data.aws_ssoadmin_instances.main.identity_store_ids[0]} --query "Users[?UserName=='${each.value.user}'].UserId" --output text > user_id_${each.key}.txt
+      aws identitystore list-users --identity-store-id ${data.aws_ssoadmin_instances.main.identity_store_ids[0]} --query "Users[?UserName=='${each.value.user}'].UserId" --output text > user_id_${each.value.user}.txt
     EOT
   }
 
   provisioner "local-exec" {
-    command = "echo user_id_${each.key}=$(cat user_id_${each.key}.txt) >> ${path.module}/user_ids.env"
+    command = "echo user_id_${each.value.user}=$(cat user_id_${each.value.user}.txt) >> ${path.module}/user_ids.env"
   }
 }
 
 data "local_file" "user_ids" {
   filename = "${path.module}/user_ids.env"
   depends_on = [null_resource.get_user_ids]
+}
+
+resource "aws_ssoadmin_account_assignment" "assignments" {
+  for_each = { for user_map in flatten(local.flattened_user_groups) : user_map.user => user_map }
+  depends_on = [data.local_file.user_ids]
+
+  instance_arn       = data.aws_ssoadmin_instances.main.arns[0]
+  permission_set_arn = data.aws_ssoadmin_permission_set.all[each.value.permission_set].arn
+  principal_type     = "USER"
+  target_id          = data.aws_organizations_organization.main.id
+  target_type        = "AWS_ACCOUNT"
+
+  principal_id = chomp(element(split("=", file("${path.module}/user_id_${each.value.user}.txt")), 1))
 }
 
 # Permission Sets
@@ -53,18 +73,6 @@ data "aws_ssoadmin_permission_set" "all" {
   name         = each.key
 }
 
-resource "aws_ssoadmin_account_assignment" "assignments" {
-  for_each = local.flattened_user_groups
-  depends_on = [data.local_file.user_ids]
-
-  instance_arn       = data.aws_ssoadmin_instances.main.arns[0]
-  permission_set_arn = data.aws_ssoadmin_permission_set.all[each.value.group].arn
-  principal_type     = "USER"
-  target_id          = data.aws_organizations_organization.main.id
-  target_type        = "AWS_ACCOUNT"
-
-  principal_id = chomp(element(split("=", file("${path.module}/user_id_${each.key}.txt")), 1))
-}
 
 
 
