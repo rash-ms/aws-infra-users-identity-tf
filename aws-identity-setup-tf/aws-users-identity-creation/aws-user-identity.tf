25aa89c6d436e9c4513f93fc78256bbe44bef9a1
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-06 01:07:17 +0100

    create aws sso for user

--- aws-identity-setup-tf/aws-users-identity-creation/aws-user-identity.tf
+++ aws-identity-setup-tf/aws-users-identity-creation/aws-user-identity.tf
@@ -1,11 +1,13 @@
 locals {
   config = yamldecode(file(var.yaml_path))
 
+  # Mapping of groups to permission sets
   group_to_permission_set = {
     "byt-data-eng-fullAccess" = "byt-data-eng-DEV-fullAccess"
     "byt-data-eng-readonly" = "byt-data-eng-PROD-readonly"
   }
 
+  # Flatten the user_groups into a single map
   flattened_user_groups = merge([
     for group_name, users in local.config : {
       for user in users : "${group_name}-${user}" => {
@@ -17,22 +19,19 @@ locals {
   ]...)
 }
 
+resource "aws_iam_identity_center_user" "users" {
+  for_each = { for k, v in local.flattened_user_groups : k => v if length(v.user) > 0 }
+
+  user_name   = each.value.user
+  first_name  = split("@", each.value.user)[0]
+  last_name   = "default"
+  display_name = each.value.user
+  email       = each.value.user
+}
+
 data "aws_ssoadmin_instances" "main" {}
 
 data "aws_organizations_organization" "main" {}
-
-# Identity Store Users
-data "aws_identitystore_user" "sso_users" {
-  for_each          = local.flattened_user_groups
-  identity_store_id = data.aws_ssoadmin_instances.main.identity_store_ids[0]
-
-  alternate_identifier {
-    external_id {
-      issuer = "aws"
-      id     = each.value.user
-    }
-  }
-}
 
 # Permission Sets
 data "aws_ssoadmin_permission_set" "all" {
@@ -50,5 +49,5 @@ resource "aws_ssoadmin_account_assignment" "assignments" {
   target_id          = data.aws_organizations_organization.main.id
   target_type        = "AWS_ACCOUNT"
 
-  principal_id = data.aws_identitystore_user.sso_users[each.key].id
+  principal_id = aws_iam_identity_center_user.users[each.key].id
 }
