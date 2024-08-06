d1fdbdb7f2976007e0b68b0cdf891f52a180db74
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-06 02:02:51 +0100

    create aws sso for user

--- aws-identity-setup-tf/aws-users-identity-creation/aws-user-identity.tf
+++ aws-identity-setup-tf/aws-users-identity-creation/aws-user-identity.tf
@@ -35,24 +35,18 @@ resource "null_resource" "get_user_ids" {
   provisioner "local-exec" {
     command = <<EOT
       user_id=$(aws identitystore list-users --identity-store-id ${data.aws_ssoadmin_instances.main.identity_store_ids[0]} --query "Users[?UserName=='${each.value.user}'].UserId" --output text)
-      echo "{\"user_id\": \"${user_id}\"}" > ${path.module}/user_id_${each.value.user}.json
+      echo ${user_id} > ${path.module}/user_id_${each.value.user}.txt
     EOT
   }
 }
 
-data "local_file" "user_ids" {
-  for_each = { for user_map in local.flattened_user_groups : user_map.user => user_map }
-  filename = "${path.module}/user_id_${each.key}.json"
-  depends_on = [null_resource.get_user_ids]
-}
-
 resource "null_resource" "add_users_to_group" {
   for_each = { for user_map in local.flattened_user_groups : user_map.user => user_map }
-  depends_on = [data.local_file.user_ids]
+  depends_on = [null_resource.get_user_ids]
 
   provisioner "local-exec" {
     command = <<EOT
-      aws identitystore create-group-membership --identity-store-id ${data.aws_ssoadmin_instances.main.identity_store_ids[0]} --group-id ${lookup(local.config, each.value.group, null)} --member-id $(jq -r .user_id ${path.module}/user_id_${each.value.user}.json)
+      aws identitystore create-group-membership --identity-store-id ${data.aws_ssoadmin_instances.main.identity_store_ids[0]} --group-id ${lookup(local.config, each.value.group, null)} --member-id $(cat ${path.module}/user_id_${each.value.user}.txt)
     EOT
   }
 }
