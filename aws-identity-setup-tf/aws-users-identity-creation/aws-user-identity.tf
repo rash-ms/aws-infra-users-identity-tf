b228c6319fd972bebb86490362ab3d20a13f7363
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-06 10:55:23 +0100

    create aws sso for user

--- aws-identity-setup-tf/aws-users-identity-creation/aws-user-identity.tf
+++ aws-identity-setup-tf/aws-users-identity-creation/aws-user-identity.tf
@@ -2,17 +2,6 @@ provider "aws" {
   region = "us-east-1"  # Replace with your desired region
 }
 
-# Ensure the directory for user ID files exists
-resource "null_resource" "create_directory" {
-  provisioner "local-exec" {
-    command = "mkdir -p ${path.module}/user_ids"
-  }
-
-  triggers = {
-    always_run = timestamp()
-  }
-}
-
 locals {
   config = yamldecode(file(var.yaml_path))
 
@@ -29,10 +18,8 @@ locals {
 
 data "aws_ssoadmin_instances" "main" {}
 
-resource "null_resource" "check_and_create_users" {
+resource "null_resource" "manage_users" {
   for_each = { for user_map in local.flattened_user_groups : user_map.user => user_map }
-
-  depends_on = [null_resource.create_directory]
 
   provisioner "local-exec" {
     command = <<EOT
@@ -40,7 +27,8 @@ resource "null_resource" "check_and_create_users" {
       if [ -z "$user_id" ]; then
         user_id=$(aws identitystore create-user --identity-store-id ${data.aws_ssoadmin_instances.main.identity_store_ids[0]} --user-name "${each.value.user}" --display-name "${each.value.user}" --name '{"FamilyName": "default", "GivenName": "${split("@", each.value.user)[0]}"}' --emails '[{"Primary": true, "Type": "work", "Value": "${each.value.user}"}]' --query "User.UserId" --output text)
       fi
-      echo "{\"user\": \"${each.value.user}\", \"user_id\": \"$user_id\"}" > ${path.module}/user_ids/user_id_${each.value.user}.json
+      group_id=$(aws identitystore list-groups --identity-store-id ${data.aws_ssoadmin_instances.main.identity_store_ids[0]} --query "Groups[?DisplayName=='${each.value.group}'].GroupId" --output text)
+      aws identitystore create-group-membership --identity-store-id ${data.aws_ssoadmin_instances.main.identity_store_ids[0]} --group-id $group_id --member-id $user_id
     EOT
 
     environment = {
@@ -53,39 +41,6 @@ resource "null_resource" "check_and_create_users" {
   triggers = {
     always_run = timestamp()
   }
-}
-
-data "local_file" "user_ids" {
-  for_each = { for user_map in local.flattened_user_groups : user_map.user => user_map }
-  filename = "${path.module}/user_ids/user_id_${each.key}.json"
-}
-
-locals {
-  user_ids = {
-    for user_map in local.flattened_user_groups :
-    user_map.user => jsondecode(data.local_file.user_ids[user_map.user].content).user_id
-  }
-}
-
-# Add users to groups
-resource "null_resource" "add_users_to_group" {
-  for_each = { for user_map in local.flattened_user_groups : user_map.user => user_map }
-
-  provisioner "local-exec" {
-    command = <<EOT
-      aws identitystore create-group-membership --identity-store-id ${data.aws_ssoadmin_instances.main.identity_store_ids[0]} --group-id ${lookup(local.config, each.value.group, null)} --member-id ${lookup(local.user_ids, each.key)}
-    EOT
-
-    environment = {
-      AWS_REGION = "us-east-1"  # Ensure the correct region is set
-    }
-
-    interpreter = ["sh", "-c"]
-  }
-}
-
-output "user_ids" {
-  value = local.user_ids
 }
 
 output "debug_mappings" {
