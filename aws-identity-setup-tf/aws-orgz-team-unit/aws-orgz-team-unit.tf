206343314b8213bb75ae9c395c5fe93738e43b83
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-07 20:24:31 +0100

    refactor aws orgz

--- aws-identity-setup-tf/aws-orgz-team-unit/aws-orgz-team-unit.tf
+++ aws-identity-setup-tf/aws-orgz-team-unit/aws-orgz-team-unit.tf
@@ -203,7 +203,8 @@ resource "aws_ssoadmin_account_assignment" "readonly_assignment" {
     for k, v in local.account_map : k => v if v.env == "PROD"
   }
   instance_arn = data.aws_ssoadmin_instances.main.arns[0]
-  permission_set_arn = aws_ssoadmin_permission_set.readonly_permission_set[each.key].arn
+  # permission_set_arn = aws_ssoadmin_permission_set.readonly_permission_set[each.key].arn
+  permission_set_arn = aws_ssoadmin_permission_set.readonly_permission_set["${each.key}_readonly"].arn
   principal_id = local.group_ids[each.key]  # Principal ID of the group
   principal_type = "GROUP"
   target_id = aws_organizations_account.team_wrkspc_account[each.key].id
@@ -215,7 +216,8 @@ resource "aws_ssoadmin_account_assignment" "full_access_assignment" {
     for k, v in local.account_map : k => v if v.env == "DEV"
   }
   instance_arn = data.aws_ssoadmin_instances.main.arns[0]
-  permission_set_arn = aws_ssoadmin_permission_set.full_access_permission_set[each.key].arn
+  # permission_set_arn = aws_ssoadmin_permission_set.full_access_permission_set[each.key].arn
+  permission_set_arn = aws_ssoadmin_permission_set.full_access_permission_set["${each.key}_fullAccess"].arn
   principal_id = local.group_ids[each.key]  # Principal ID of the group
   principal_type = "GROUP"
   target_id = aws_organizations_account.team_wrkspc_account[each.key].id
