5ea79cc9bee9df387f2fac835f6b3642648ead78
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-07 20:19:35 +0100

    refactor aws orgz

--- aws-identity-setup-tf/aws-orgz-team-unit/aws-orgz-team-unit.tf
+++ aws-identity-setup-tf/aws-orgz-team-unit/aws-orgz-team-unit.tf
@@ -52,7 +52,7 @@ locals {
 
   group_ids = {
     for group, group_name in local.policy_group_mapping :
-    group_name => split("/", aws_identitystore_group.team_group[group_name].group_id)[1]
+    group => split("/", aws_identitystore_group.team_group[group_name].id)[1]
   }
 
 }
@@ -113,17 +113,23 @@ data "aws_ssoadmin_instances" "main" {}
 # }
 
 resource "aws_identitystore_group" "team_group" {
-  for_each = local.group_mappings
-  identity_store_id  = tolist(data.aws_ssoadmin_instances.main.identity_store_ids)[0]
-  display_name = each.value.group
-
-  # alternate_identifier {
-  #   unique_attribute {
-  #     attribute_path = "DisplayName"
-  #     attribute_value = each.value.group
-  #   }
-  # }
+  for_each = { for k, v in local.group_mappings : v.group => k }
+  identity_store_id = tolist(data.aws_ssoadmin_instances.main.identity_store_ids)[0]
+  display_name      = each.key
 }
+
+# resource "aws_identitystore_group" "team_group" {
+#   for_each = local.group_mappings
+#   identity_store_id  = tolist(data.aws_ssoadmin_instances.main.identity_store_ids)[0]
+#   display_name = each.value.group
+
+#   # alternate_identifier {
+#   #   unique_attribute {
+#   #     attribute_path = "DisplayName"
+#   #     attribute_value = each.value.group
+#   #   }
+#   # }
+# }
 
 resource "aws_ssoadmin_permission_set" "readonly_permission_set" {
   for_each     = local.readonly_permission_sets
