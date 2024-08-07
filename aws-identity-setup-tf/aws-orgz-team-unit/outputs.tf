f61ec044c71be6ea19175c503b40ef903efe070c
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-07 19:02:15 +0100

    refactor aws orgz

--- aws-identity-setup-tf/aws-orgz-team-unit/outputs.tf
+++ aws-identity-setup-tf/aws-orgz-team-unit/outputs.tf
@@ -11,9 +11,13 @@ output "account_map" {
   value = local.account_map
 }
 
-output "team_group_ids" {
-  value = { for k, v in aws_identitystore_group.team_group : k => v.group_id }
+output "group_id" {
+  value = aws_identitystore_group.team_group.group_id
 }
+
+# output "team_group_ids" {
+#   value = { for k, v in aws_identitystore_group.team_group : k => v.group_id }
+# }
 
 
 # output "team_group_ids" {
