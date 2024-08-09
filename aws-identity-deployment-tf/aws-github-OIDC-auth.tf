bd49932c6ed00adc9e4bc752f53ff0df553b92c0
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-09 19:47:18 +0100

    refactoring my aws org tf

--- aws-identity-deployment-tf/aws-github-OIDC-auth.tf
+++ aws-identity-deployment-tf/aws-github-OIDC-auth.tf
@@ -1,11 +1,11 @@
 module "aws-github-OIDC-auth" {
-  source             = "../aws-identity-setup-tf/aws-github-OIDC-auth"
-  github_action_name    = var.github_action_name
+  source                  = "../aws-identity-setup-tf/aws-github-OIDC-auth"
+  github_action_name      = var.github_action_name
   github_action_role_tags = var.github_action_role_tags
 
-  providers = {
-    aws = aws.aws-us-east-1
-  }
+  # providers = {
+  #   aws = aws.aws-us-east-1
+  # }
 }
 
 
--- aws-identity-deployment-tf/aws-users-identity.tf
+++ aws-identity-deployment-tf/aws-users-identity.tf
@@ -1,6 +1,6 @@
-provider "aws" {
-  region = "us-east-1"  
-}
+# provider "aws" {
+#   region = "us-east-1"  
+# }
 
 module "identity" {
   source    = "../aws-identity-setup-tf/aws-users-identity-creation"
