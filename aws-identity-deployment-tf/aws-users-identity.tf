47ff423ed6257b84bc50eea3738a32e0edf51b93
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-09 17:02:30 +0100

    refactoring my aws org tf

--- aws-identity-deployment-tf/aws-users-identity.tf
+++ aws-identity-deployment-tf/aws-users-identity.tf
@@ -1,20 +1,20 @@
-# provider "aws" {
-#   region = "us-east-1"  
-# }
+provider "aws" {
+  region = "us-east-1"  
+}
 
-# module "identity" {
-#   source    = "../aws-identity-setup-tf/aws-users-identity-creation"
-#   # users_yaml_path = "${path.module}/users.yaml"
-#   # groups_yaml_path = "${path.module}/groups.yaml"
-#   users_yaml_path = "../aws-identity-setup-tf/aws-users-identity-creation/base_conf/users.yaml"
-#   groups_yaml_path = "../aws-identity-setup-tf/aws-users-identity-creation/base_conf/groups.yaml"
-# }
+module "identity" {
+  source    = "../aws-identity-setup-tf/aws-users-identity-creation"
+  # users_yaml_path = "${path.module}/users.yaml"
+  # groups_yaml_path = "${path.module}/groups.yaml"
+  users_yaml_path = "../aws-identity-setup-tf/aws-users-identity-creation/base_conf/users.yaml"
+  groups_yaml_path = "../aws-identity-setup-tf/aws-users-identity-creation/base_conf/groups.yaml"
+}
 
 
-# output "created_users" {
-#   value = module.identity.created_users
-# }
+output "created_users" {
+  value = module.identity.created_users
+}
 
-# output "group_memberships" {
-#   value = module.identity.group_memberships
-# }
+output "group_memberships" {
+  value = module.identity.group_memberships
+}
