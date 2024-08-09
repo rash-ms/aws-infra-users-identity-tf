41debc47869a4c605356e682dc5655b4cb2ee5c2
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-09 19:51:18 +0100

    refactoring my aws org tf

--- aws-identity-deployment-tf/aws-github-OIDC-auth.tf
+++ aws-identity-deployment-tf/aws-github-OIDC-auth.tf
@@ -2,10 +2,6 @@ module "aws-github-OIDC-auth" {
   source                  = "../aws-identity-setup-tf/aws-github-OIDC-auth"
   github_action_name      = var.github_action_name
   github_action_role_tags = var.github_action_role_tags
-
-  # providers = {
-  #   aws = aws.aws-us-east-1
-  # }
 }
 
 
--- aws-identity-setup-tf/aws-users-identity-creation/outputs.tf
+++ aws-identity-setup-tf/aws-users-identity-creation/outputs.tf
@@ -2,6 +2,6 @@ output "created_users" {
   value = aws_identitystore_user.users
 }
 
-# output "group_memberships" {
-#   value = aws_identitystore_group_membership.memberships
-# }
+output "group_memberships" {
+  value = aws_identitystore_group_membership.memberships
+}
