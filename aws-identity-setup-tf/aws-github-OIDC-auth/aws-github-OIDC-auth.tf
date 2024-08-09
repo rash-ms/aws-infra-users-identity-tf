ad879c8ed13b4a88f09d9b1b63528ae368f78bd9
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-09 23:07:45 +0100

    refactoring my aws org tf

--- aws-identity-setup-tf/aws-github-OIDC-auth/aws-github-OIDC-auth.tf
+++ aws-identity-setup-tf/aws-github-OIDC-auth/aws-github-OIDC-auth.tf
@@ -28,6 +28,10 @@ resource "aws_iam_openid_connect_provider" "github_oidc_deployment" {
   thumbprint_list = ["1b511abead59c6ce207077c0bf0e0043b1382612"]
 
   provider = aws.default
+
+  lifecycle {
+    ignore_changes = all
+  }
 }
 
 resource "aws_iam_role" "roles" {
