e13c3935a576113d7b7f3bfff3b7a00ace41f71a
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-10 15:42:41 +0100

    refactoring my aws org tf

--- aws-identity-setup-tf/aws-github-OIDC-auth/aws-github-OIDC-auth.tf
+++ aws-identity-setup-tf/aws-github-OIDC-auth/aws-github-OIDC-auth.tf
@@ -123,10 +123,10 @@
 # }
 
 
-provider "aws" {
-  alias  = "default"
-  region = "us-east-1"
-}
+# provider "aws" {
+#   alias  = "default"
+#   region = "us-east-1"
+# }
 
 # locals {
 #   deployments = jsondecode(file("${path.module}/github-deployment.json"))
