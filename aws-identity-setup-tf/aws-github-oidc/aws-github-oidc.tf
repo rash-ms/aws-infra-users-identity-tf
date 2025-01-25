c66ff216096af977024530f7640155e973476b39
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2025-01-25 22:39:54 +0000

    test oidc

--- aws-identity-setup-tf/aws-github-oidc/aws-github-oidc.tf
+++ aws-identity-setup-tf/aws-github-oidc/aws-github-oidc.tf
@@ -7,51 +7,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# resource "aws_iam_openid_connect_provider" "github_oidc" {
-#   for_each = { for account in local.accounts : account.account_id => account }
-
-#   url             = "https://token.actions.githubusercontent.com"
-#   client_id_list  = ["sts.amazonaws.com"]
-#   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
-
-#   lifecycle {
-#     ignore_changes = [url, client_id_list, thumbprint_list]
-#   }
-# }
-
-
-# Create OIDC provider only if it doesn't exist
 resource "aws_iam_openid_connect_provider" "github_oidc" {
-  for_each = {
-    for account in local.accounts : account.account_id => account
-    if try(data.aws_iam_openid_connect_provider.github_oidc[account.account_id].arn, null) == null
-  }
+  for_each = { for account in local.accounts : account.account_id => account }
 
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+ 
+  depends_on = [aws_iam_role.github_role]
 
-  lifecycle {
-    ignore_changes = [url, client_id_list, thumbprint_list]
-  }
+  # lifecycle {
+  #   ignore_changes = [url, client_id_list, thumbprint_list]
+  # }
+  
 }
 
-# Pre-check for existing OIDC provider
-data "aws_iam_openid_connect_provider" "github_oidc" {
-  for_each = { for account in local.accounts : account.account_id => account }
 
-  url = "https://token.actions.githubusercontent.com"
-}
-
-locals {
-  oidc_provider_arns = {
-    for account_id, account in local.accounts :
-    account_id => try(
-      data.aws_iam_openid_connect_provider.github_oidc[account_id].arn,
-      aws_iam_openid_connect_provider.github_oidc[account_id].arn
-    )
-  }
-}
 
 
 
@@ -66,8 +37,7 @@ resource "aws_iam_role" "github_role" {
       {
         Effect = "Allow",
         Principal = {
-          # Federated = aws_iam_openid_connect_provider.github_oidc[each.key].arn
-          Federated = local.oidc_provider_arns[each.key]
+          Federated = aws_iam_openid_connect_provider.github_oidc[each.key].arn
         },
         Action = "sts:AssumeRoleWithWebIdentity",
         Condition = {
