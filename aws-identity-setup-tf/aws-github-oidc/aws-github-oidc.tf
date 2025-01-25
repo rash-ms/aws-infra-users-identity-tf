61352ec62a53053dab5d055d46ada3540a17b160
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2025-01-25 22:03:20 +0000

    test oidc

--- aws-identity-setup-tf/aws-github-oidc/aws-github-oidc.tf
+++ aws-identity-setup-tf/aws-github-oidc/aws-github-oidc.tf
@@ -1,14 +1,12 @@
 locals {
-  # Parse the JSON file and access the `result` key
-  accounts = jsondecode(file("${path.module}/account_github_oidc.json")).result
+  # Directly parse the JSON file
+  accounts = jsondecode(file("${path.module}/account_github_oidc.json"))
 }
 
-# Single AWS provider
 provider "aws" {
   region = "us-east-1"
 }
 
-# OIDC Provider for GitHub Actions
 resource "aws_iam_openid_connect_provider" "github_oidc" {
   for_each = { for account in local.accounts : account.account_id => account }
 
@@ -17,7 +15,6 @@ resource "aws_iam_openid_connect_provider" "github_oidc" {
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
-# IAM Role for GitHub Actions
 resource "aws_iam_role" "github_role" {
   for_each = { for account in local.accounts : account.account_id => account }
 
@@ -45,35 +42,6 @@ resource "aws_iam_role" "github_role" {
   })
 }
 
-
-# resource "aws_iam_role" "github_role" {
-#   for_each = { for account in local.accounts : account.account_id => account }
-
-#   name = "GitHubActionsRole-${each.value.environment}"
-
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#         {
-#             Effect = "Allow",
-#             Principal = {
-#               Federated = aws_iam_openid_connect_provider.github_oidc[each.key].arn
-#             },
-#             Action = "sts:AssumeRoleWithWebIdentity",
-#             Condition = {
-#                 StringEquals = {
-#                     "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
-#                 },
-#                 StringLike = {
-#                     "token.actions.githubusercontent.com:sub": each.value.repo_sub
-#                 }
-#             }
-#         }
-#     ]
-# })
-# }
-
-# Attach Administrator Access Policy to the IAM Role
 resource "aws_iam_role_policy_attachment" "attach_admin_policy" {
   for_each = { for account in local.accounts : account.account_id => account }
 
