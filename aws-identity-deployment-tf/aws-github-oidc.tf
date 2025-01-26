a1977415c9509e8970e96cd897447df81a402171
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2025-01-26 00:04:09 +0000

    test oidc

--- aws-identity-deployment-tf/aws-github-oidc.tf
+++ aws-identity-deployment-tf/aws-github-oidc.tf
@@ -1,3 +1,26 @@
-module "iam_oidc_deployment" {
+# module "iam_oidc_deployment" {
+#   source = "../aws-identity-setup-tf/aws-github-oidc"
+# }
+
+locals {
+  # Directly parse the JSON file
+  accounts = jsondecode(file("${path.module}/account_github_oidc.json"))
+}
+
+module "aws_oidc_providers" {
+  for_each = {
+    for account in local.accounts : account.account_id => {
+      account_id = account.account_id,
+      region     = account.region,
+      role_name  = "byt-data-org-${account.environment}-role",
+      repo_sub   = account.repo_sub
+    }
+  }
+
+  # source     = "./modules/aws_oidc_provider"
   source = "../aws-identity-setup-tf/aws-github-oidc"
+  account_id = each.value.account_id
+  region     = each.value.region
+  role_name  = each.value.role_name
+  repo_sub   = each.value.repo_sub
 }
--- aws-identity-setup-tf/aws-github-oidc/aws-github-oidc.tf
+++ aws-identity-setup-tf/aws-github-oidc/aws-github-oidc.tf
@@ -3,22 +3,22 @@ locals {
   accounts = jsondecode(file("${path.module}/account_github_oidc.json"))
 }
 
-module "aws_oidc_providers" {
-  for_each = {
-    for account in local.accounts : account.account_id => {
-      account_id = account.account_id,
-      region     = account.region,
-      role_name  = "byt-data-org-${account.environment}-role",
-      repo_sub   = account.repo_sub
-    }
-  }
+# module "aws_oidc_providers" {
+#   for_each = {
+#     for account in local.accounts : account.account_id => {
+#       account_id = account.account_id,
+#       region     = account.region,
+#       role_name  = "byt-data-org-${account.environment}-role",
+#       repo_sub   = account.repo_sub
+#     }
+#   }
 
-  source     = "./modules/aws_oidc_provider"
-  account_id = each.value.account_id
-  region     = each.value.region
-  role_name  = each.value.role_name
-  repo_sub   = each.value.repo_sub
-}
+#   source     = "./modules/aws_oidc_provider"
+#   account_id = each.value.account_id
+#   region     = each.value.region
+#   role_name  = each.value.role_name
+#   repo_sub   = each.value.repo_sub
+# }
 
 
 provider "aws" {
