8dce29f5c29ce74a46163adef83eb4b51c20c26a
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2025-01-26 14:31:21 +0000

    test oidc

--- aws-identity-deployment-tf/aws-github-oidc.tf
+++ aws-identity-deployment-tf/aws-github-oidc.tf
@@ -3,6 +3,11 @@ module "iam_oidc_deployment" {
   environment = var.environment
 }
 
+module "iam_oidc_deployment" {
+  source = "../aws-identity-setup-tf/_aws-github-oidc"
+  environment = var.environment
+}
+
 variable "environment" {
   description = "Environment to deploy to (dev or prod)"
   type        = string
