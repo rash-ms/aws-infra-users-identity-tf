5beae6cee632fffc268765242a7783ef92971114
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2025-01-27 00:25:59 +0000

    add

--- aws-identity-setup-tf/aws-users-identity-creation/variables.tf
+++ aws-identity-setup-tf/aws-users-identity-creation/variables.tf
@@ -1,9 +1,23 @@
+# variable "users_yaml_path" {
+#   description = "Path to the users YAML configuration file"
+#   type        = string
+# }
+
+# variable "groups_yaml_path" {
+#   description = "Path to the groups YAML configuration file"
+#   type        = string
+# }
+
+
+
 variable "users_yaml_path" {
   description = "Path to the users YAML configuration file"
   type        = string
+  default     = "../aws-identity-setup-tf/aws-users-identity-creation/base_conf/users.yaml"
 }
 
 variable "groups_yaml_path" {
   description = "Path to the groups YAML configuration file"
   type        = string
+  default     = "../aws-identity-setup-tf/aws-users-identity-creation/base_conf/groups.yaml"
 }
