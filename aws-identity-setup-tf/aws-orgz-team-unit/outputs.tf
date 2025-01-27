42a75221757e7ed2a80d39285688758e56091db5
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2025-01-27 22:44:13 +0000

    create data-platform orgz

--- aws-identity-setup-tf/aws-orgz-team-unit/outputs.tf
+++ aws-identity-setup-tf/aws-orgz-team-unit/outputs.tf
@@ -1,15 +1,15 @@
-# output "organization_id" {
-#   value = aws_organizations_organization.org.id
+# # output "organization_id" {
+# #   value = aws_organizations_organization.org.id
+# # }
+
+# output "account_map" {
+#   value = local.account_map
 # }
 
-output "account_map" {
-  value = local.account_map
-}
+# output "team_wrkspc_account_ids" {
+#   value = { for k, v in aws_organizations_account.team_wrkspc_account : k => v.id }
+# }
 
-output "team_wrkspc_account_ids" {
-  value = { for k, v in aws_organizations_account.team_wrkspc_account : k => v.id }
-}
-
-output "team_group_ids" {
-  value = { for k, v in aws_identitystore_group.team_group : k => v.group_id }
-}
+# output "team_group_ids" {
+#   value = { for k, v in aws_identitystore_group.team_group : k => v.group_id }
+# }
