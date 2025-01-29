f29c04f29a00dc30c7fdeb11506122310d2b0452
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2025-01-29 21:36:15 +0000

    create data-platform orgz

--- aws-identity-setup-tf/aws-orgz-team-unit/aws-orgz-team-unit.tf
+++ aws-identity-setup-tf/aws-orgz-team-unit/aws-orgz-team-unit.tf
@@ -74,13 +74,6 @@ resource "aws_organizations_account" "accounts" {
   }
 }
 
-# # ✅ Create Identity Store Groups dynamically for each team
-# resource "aws_identitystore_group" "groups" {
-#   for_each          = local.group_mappings
-#   identity_store_id = tolist(data.aws_ssoadmin_instances.main.identity_store_ids)[0]
-#   display_name      = "${each.key}-${var.environment}"
-#   description       = "Access group for ${each.key}"
-# }
 
 # ✅ Create Identity Store Groups with Unique Names
 resource "aws_identitystore_group" "groups" {
@@ -89,7 +82,6 @@ resource "aws_identitystore_group" "groups" {
   display_name      = "${each.key}-${var.environment}-group"
   description       = "Access group for ${each.key} in ${var.environment}"
 }
-
 
 
 # ✅ Create Permission Sets with Unique Names
@@ -125,29 +117,7 @@ resource "aws_ssoadmin_account_assignment" "group_assignment" {
 }
 
 
-# ✅ Create Identity Store Groups dynamically
-resource "aws_identitystore_group" "groups" {
-  for_each          = local.group_mappings
-  identity_store_id = tolist(data.aws_ssoadmin_instances.main.identity_store_ids)[0]
-  display_name      = "${each.key}-${var.environment}"
-  description       = "Access group for ${each.key}"
-}
 
-
-# ✅ Assign Permission Sets to Accounts
-# resource "aws_ssoadmin_account_assignment" "group_assignment" {
-#   for_each = {
-#     for key, value in local.group_mappings :
-#     "${key}-${value.policy_name}-${var.environment}" => value
-#   }
-
-#   instance_arn       = tolist(data.aws_ssoadmin_instances.main.arns)[0]
-#   permission_set_arn = aws_ssoadmin_permission_set.policy_set["${var.environment}-${each.value.policy_name}"].arn
-#   principal_id       = aws_identitystore_group.groups[each.key].group_id
-#   principal_type     = "GROUP"
-#   target_id          = aws_organizations_account.accounts[each.key].id
-#   target_type        = "AWS_ACCOUNT"
-# }
 
 
 
