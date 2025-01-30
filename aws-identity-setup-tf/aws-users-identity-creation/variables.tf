a8a0c24b713d373bf4107f76f8d96bcaee914dbb
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2025-01-30 00:33:02 +0000

    create data-platform orgz

--- aws-identity-setup-tf/aws-users-identity-creation/variables.tf
+++ aws-identity-setup-tf/aws-users-identity-creation/variables.tf
@@ -1,15 +1,3 @@
-# variable "users_yaml_path" {
-#   description = "Path to the users YAML configuration file"
-#   type        = string
-# }
-
-# variable "groups_yaml_path" {
-#   description = "Path to the groups YAML configuration file"
-#   type        = string
-# }
-
-
-
 variable "users_yaml_path" {
   description = "Path to the users YAML configuration file"
   type        = string
