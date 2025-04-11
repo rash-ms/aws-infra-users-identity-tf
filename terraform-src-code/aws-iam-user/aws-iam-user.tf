460ab90c1d3cb5d311dc23c5f82b88bfb0261c10
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2025-04-11 23:53:19 +0100

    create iam user

--- terraform-src-code/aws-iam-user/aws-iam-user.tf
+++ terraform-src-code/aws-iam-user/aws-iam-user.tf
@@ -1,3 +1,36 @@
+data "local_file" "iam_users_config" {
+  filename = var.iam_users_yaml_path
+}
+
+locals {
+  iam_user_config = yamldecode(data.local_file.iam_users_config.content)
+
+  selected_usernames = try(
+    lookup(local.iam_user_config, var.environment)["users"],
+    []
+  )
+}
+
+resource "aws_iam_user" "dev_users" {
+  provider = aws.dev
+  count    = var.environment == "dev" ? length(local.selected_usernames) : 0
+
+  name = local.selected_usernames[count.index]
+}
+
+resource "aws_iam_user" "prod_users" {
+  provider = aws.prod
+  count    = var.environment == "prod" ? length(local.selected_usernames) : 0
+
+  name = local.selected_usernames[count.index]
+}
+
+output "created_users" {
+  value = local.selected_usernames
+}
+
+
+
 # locals {
 #   dev_usernames  = ["byt-test-cicd-iam-user-dev", "byt-test-s3-iam-user-dev"]
 #   prod_usernames = ["byt-test-cicd-iam-user-prod", "byt-test-s3-iam-user-prod"]
@@ -35,35 +68,3 @@
 
 #   name = each.value
 # }
-
-
-data "local_file" "iam_users_config" {
-  filename = var.iam_users_yaml_path
-}
-
-locals {
-  iam_user_config = yamldecode(data.local_file.iam_users_config.content)
-
-  selected_usernames = lookup(local.iam_user_config, var.environment, {})["users"]
-}
-
-# resource "aws_iam_user" "users" {
-#   provider = var.environment == "dev" ? aws.dev : aws.prod
-#   for_each = toset(local.selected_usernames)
-
-#   name = each.value
-# }
-
-resource "aws_iam_user" "dev_users" {
-  provider = aws.dev
-  count    = var.environment == "dev" ? length(local.selected_usernames) : 0
-
-  name = local.selected_usernames[count.index]
-}
-
-resource "aws_iam_user" "prod_users" {
-  provider = aws.prod
-  count    = var.environment == "prod" ? length(local.selected_usernames) : 0
-
-  name = local.selected_usernames[count.index]
-}
