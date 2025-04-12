2e373be4add508c3bb17d6f2ca3b5f63a4bdb980
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2025-04-12 10:50:32 +0100

    create oidc deployer

--- terraform-src-code/aws-github-oidc/providers.tf
+++ terraform-src-code/aws-github-oidc/providers.tf
@@ -2,12 +2,3 @@ terraform {
   required_version = ">=v0.14.7"
   backend "s3" {}
 }
-
-provider "aws" {
-  alias  = "target"
-  region = "us-east-1"
-
-  assume_role {
-    role_arn = local.config.target_role_arn
-  }
-}
